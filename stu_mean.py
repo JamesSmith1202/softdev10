2188c46761d7ba94cecdb2775685ee883b8bbaff
imahdi2000 <imahdi@stuy.edu>
2017-10-17 14:39:27 -0400

    hi

--- stu_mean.py
+++ stu_mean.py
@@ -1,6 +1,7 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
+
 f="discobandit.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
@@ -8,25 +9,39 @@ c = db.cursor()    #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
+csv_with_students_info = csv.DictReader(open("peeps.csv"))
+csv_with_course_info= csv.DictReader(open("courses.csv"))
+#===========================================================
+c.execute('CREATE TABLE students (name str, age INTEGER, id INTEGER);')
+for row in csv_with_students_info:
+    c.execute("INSERT INTO students VALUES (" + "'"+row["name"]+"'," + row["age"] + "," + row["id"] + ");")
 
-def populate(executable, fileName, tableName):
-    with open(fileName, 'rb') as csvfile0:
-        reader = csv.reader(csvfile0)#get a normal line reader
-        headers = reader.next()#get a list of the headers
-    with open(fileName) as csvfile1:
-        reader = csv.DictReader(csvfile1)#swap to read as dict
-        for row in reader:
-            exestr = "INSERT INTO " + tableName + " VALUES ( \""+row[headers[0]]+"\", \""+row[headers[1]]+"\", \""+row[headers[2]]+"\");"
-            executable.execute(exestr)
+c.execute('CREATE TABLE courses (code str, mark NUMERIC, id INTEGER);')
+for row in csv_with_course_info:
+    code = row['code']
+    mark = row['mark']
+    ida = row['id']
+    string = "'" +code+"'" + ',' + str(mark) + ',' +  str(ida)
+    c.execute('INSERT INTO students VALUES ('+ string +');')
 
-c.execute("CREATE TABLE courses ( code TEXT, mark INTEGER, id INTEGER);")
-c.execute("CREATE TABLE peeps ( name TEXT, age INTEGER, id INTEGER);")
-
-populate(c, "courses.csv", "courses")
-populate(c, "peeps.csv", "peeps")
+#command = ""          #put SQL statement in this string
+#c.execute(command)    #run SQL statement
 
 #==========================================================
 db.commit() #save changes
 db.close()  #close database
+print "Done"
 
+#GRAVE
+'''
+c.execute('CREATE TABLE students (name TEXT, age INTEGER, id NUMERIC );')
+for row in csv_with_students_info:
+    #print int(row["age"]) + 100
+    age = str(row["age"])
+    name = str(row["name"])
+    id = str(row["id"])
+#    c.execute('INSERT INTO students VALUES ('+ age +','+ name +','+ id +');')
+    c.execute('INSERT INTO students VALUES ('+ 'name'+',7,8);') #% (name)
 
+#===========================================================
+c.execute('CREATE TABLE courses (code TEXT, mark NUMERIC, id NUMERIC );')'''
