d9e16d25b046bd5ae35d2c8c6c063a059baeb4a0
imahdi2000 <imahdi@stuy.edu>
2017-10-17 22:10:16 -0400

    done

--- stu_mean.py
+++ stu_mean.py
@@ -9,39 +9,100 @@ c = db.cursor()    #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
-csv_with_students_info = csv.DictReader(open("peeps.csv"))
-csv_with_course_info= csv.DictReader(open("courses.csv"))
+def populate():
+
+    csv_with_students_info = csv.DictReader(open("peeps.csv"))
+    csv_with_course_info= csv.DictReader(open("courses.csv"))
+
+    c.execute('CREATE TABLE students (name str, age INTEGER, id INTEGER);')
+    for row in csv_with_students_info:
+        c.execute("INSERT INTO students VALUES (" + "'"+row["name"]+"'," + row["age"] + "," + row["id"] + ");")
+
+    c.execute('CREATE TABLE courses (code str, mark NUMERIC, id INTEGER);')
+    for row in csv_with_course_info:
+        c.execute("INSERT INTO courses VALUES (" + "'"+row["code"]+"'," + row["mark"] + "," + row["id"] + ");")
+
+    db.commit()
+
+
 #===========================================================
-c.execute('CREATE TABLE students (name str, age INTEGER, id INTEGER);')
-for row in csv_with_students_info:
-    c.execute("INSERT INTO students VALUES (" + "'"+row["name"]+"'," + row["age"] + "," + row["id"] + ");")
+def see_students_grades():
+    display = c.execute("SELECT name,mark FROM STUDENTS,COURSES WHERE students.id = courses.id")
+    for row in display:
+        print row
 
-c.execute('CREATE TABLE courses (code str, mark NUMERIC, id INTEGER);')
-for row in csv_with_course_info:
-    code = row['code']
-    mark = row['mark']
-    ida = row['id']
-    string = "'" +code+"'" + ',' + str(mark) + ',' +  str(ida)
-    c.execute('INSERT INTO students VALUES ('+ string +');')
 
-#command = ""          #put SQL statement in this string
-#c.execute(command)    #run SQL statement
+def find_freqency(): #finds the freqency of a paticular id
+
+    table = c.execute("SELECT students.id FROM STUDENTS,COURSES WHERE students.id = courses.id")
+    list = []
+    prevnum = 1
+    counter = 0
+    for iden in table:
+        if prevnum == iden[0]:
+            counter = counter + 1
+        else:
+            list.append(counter)
+            counter = 1
+            prevnum = iden[0]
+    list.append(counter)
+    return list
+
+def calculate_avg(): # finds average of all students
+    grade_list = c.execute("SELECT mark FROM STUDENTS,COURSES WHERE students.id = courses.id")
+    list = []
+    for row in grade_list:
+        list.append(row[0])
+    freq = find_freqency()
+    avg = []
+#    print freq
+#    print list
+#    print "#########################################"
+    for i in freq:
+        num = 0
+        #print i
+        for n in range(0,i):
+        #    print "---- list n -----"
+        #    print list[n]
+             num = num + list.pop(0)
+        #     print list
+        #     print num
+        #print "---final------"
+        avg.append(num/i)
+    return avg
+
+def peeptable():
+        c.execute('CREATE TABLE peeps_avg(id INTEGER, average INTEGER);')
+        avg_list = calculate_avg()
+    #    print avg_list
+        for i in range(0,len(avg_list)):
+            num = i + 1
+            value = avg_list[i]
+        #    print value
+        #    print "-----------"
+            c.execute("INSERT INTO peeps_avg VALUES (" + str(num) + "," + str(value) + ");")
+            db.commit()
+    #    print "INSERT INTO peeps_avg VALUES (" + str(num) + "," + str(value) + ";"
+
+    #    c.execute("INSERT INTO students VALUES (" + "'"+row["name"]+"'," + row["age"] + "," + row["id"] + ");")
+
+def display():
+    table = c.execute("SELECT name,peeps_avg.id,average FROM STUDENTS,peeps_avg  WHERE peeps_avg.id = students.id;")
+    print "NAME | ID | AVERAGE"
+    for i in table:
+        print i[0] + "|" + str(i[1]) + "|" + str(i[2]) + "\n"
+    return table
+
+
+
+
+
+
+
 
 #==========================================================
-db.commit() #save changes
-db.close()  #close database
-print "Done"
-
-#GRAVE
-'''
-c.execute('CREATE TABLE students (name TEXT, age INTEGER, id NUMERIC );')
-for row in csv_with_students_info:
-    #print int(row["age"]) + 100
-    age = str(row["age"])
-    name = str(row["name"])
-    id = str(row["id"])
-#    c.execute('INSERT INTO students VALUES ('+ age +','+ name +','+ id +');')
-    c.execute('INSERT INTO students VALUES ('+ 'name'+',7,8);') #% (name)
-
-#===========================================================
-c.execute('CREATE TABLE courses (code TEXT, mark NUMERIC, id NUMERIC );')'''
+populate()
+peeptable()
+display()
+db.close()
+print "Donee"
